d9fdee4426789765611927401cd43c195026d245
Paulo Perfeto <paulo.perfetto@gmail.com>
2022-03-12 13:49:49 -0300

    commit on master

--- forca.c
+++ forca.c
@@ -8,18 +8,20 @@ int main() {
 
 	int acertou = 0;
 	int enforcou = 0;
+    int tentativas = 0;
+    char chutes[26];
 
+    
 	do {
+        
+        for(int i = 0; i < strlen(palavrasecreta); i++) {
+            printf("_ ");
+        }
+        printf("\n\n");
 		char chute;
 		
 		printf("Qual letra? ");
 		scanf(" %c", &chute);
-
-		for(int i = 0; i < strlen(palavrasecreta); i++) {
-			if(palavrasecreta[i] == chute) {
-				printf("A posição %d tem essa letra\n", i+1);
-			}
-		}
-	} while (!acertou && !enforcou);
-
-}
+		
+		} while (!acertou && !enforcou);
+} 
